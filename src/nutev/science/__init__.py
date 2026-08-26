6f4810276216bc7723fed2b5863c4519aabecff2
WillianVagner123 <wvagners@hotmail.com>
2026-08-26 20:35:58 -0300

    feat(science): expose screening decision API

--- src/nutev/science/__init__.py
+++ src/nutev/science/__init__.py
@@ -15,10 +15,14 @@ from .models import (
     RecommendationCandidate,
     ResearchQuestion,
     ScientificEvent,
+    ScreeningDecision,
+    ScreeningDecisionValue,
+    ScreeningStage,
     SearchCase,
     ValidationDecision,
     derive_prisma_counts,
 )
+from .screening import events_from_screening_decision
 
 __all__ = [
     "ClaimEvaluation",
@@ -34,9 +38,13 @@ __all__ = [
     "ResearchQuestion",
     "ScientificEvent",
     "ScientificExportError",
+    "ScreeningDecision",
+    "ScreeningDecisionValue",
+    "ScreeningStage",
     "SearchCase",
     "ValidationDecision",
     "derive_prisma_counts",
+    "events_from_screening_decision",
     "reference_to_scientific_objects",
     "run_scientific_export",
 ]
